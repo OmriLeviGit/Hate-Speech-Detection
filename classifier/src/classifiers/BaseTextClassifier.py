added fix attempt, bert maximizes for f1, minor changes, attempting to check why the precision is low

classifier/src/classifiers/BaseTextClassifier.py
@@ -29,22 +29,13 @@ class BaseTextClassifier(ABC):
         self.model_name = None
 
     def load_data(self, file_name='results.csv', debug=False) -> dict[str, list]:
-        """
-        Load data
-
-        Args:
-            file_name: Name of the file under 'src'
-            debug: Use test dataset if True
-        """
         if debug:
             print("loading with 'debug' dataset")
             return self._initialize_test_dataset()
 
-        # Load data from CSV
         script_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
         df = pd.read_csv(os.path.join(script_dir, file_name))
 
-        # Extract raw data
         data = {
             'antisemitic': df[df['sentiment'] == 'Positive']['content'].tolist(),
             'not_antisemitic': df[df['sentiment'] == 'Negative']['content'].tolist(),
@@ -53,8 +44,35 @@ class BaseTextClassifier(ABC):
 
         return data
 
-    def prepare_dataset(self, raw_data: dict[str, list[str]], test_size=0.2, irrelevant_ratio=0.33,
-                                augment_ratio=0, balance_pct=None, balance_classes=False) -> tuple[
+    def prepare_dataset_old(self, datasets: dict[str, list[str]], test_size = 0.2) -> tuple[list[str], list[str], list[str], list[str]]:
+        """Prepare and split into train and test sets"""
+        posts = []
+        labels = []
+
+        for label_name, post_list in datasets.items():
+            if label_name == 'irrelevant':
+                continue
+
+            for post in post_list:
+                posts.append(post)
+                labels.append(label_name)
+
+        X = np.array(posts)
+        y = np.array(labels)
+
+        X_shuffled, y_shuffled = shuffle(X, y, random_state=self.seed)
+
+        X_train, X_test, y_train, y_test = train_test_split(
+            X_shuffled, y_shuffled,
+            test_size=test_size,
+            stratify=y_shuffled,
+            random_state=self.seed,
+        )
+
+        return X_train.tolist(), X_test.tolist(), y_train.tolist(), y_test.tolist()
+
+    def prepare_dataset(self, raw_data: dict[str, list[str]], test_size=0.2, irrelevant_ratio=0,
+                        augment_ratio=0, balance_pct=None, balance_classes=False) -> tuple[
         list[str], list[str], list[str], list[str]]:
         """
         Prepare and split into train and test sets based on the number of labels
@@ -83,7 +101,7 @@ class BaseTextClassifier(ABC):
                 balance_classes=balance_classes
             )
 
-    def _prepare_binary_dataset(self, raw_data: dict[str, list[str]], test_size=0.2, irrelevant_ratio=0.33,
+    def _prepare_binary_dataset(self, raw_data: dict[str, list[str]], test_size=0.2, irrelevant_ratio=0,
                                 augment_ratio=0, balance_pct=None) -> tuple[
         list[str], list[str], list[str], list[str]]:
         """

classifier/src/classifiers/BertClassifier.py
@@ -5,7 +5,7 @@ import numpy as np
 import optuna
 import torch
 from torch.utils.data import Dataset
-from sklearn.metrics import accuracy_score, f1_score
+from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score
 from sklearn.model_selection import KFold
 from transformers import (
     AutoModelForSequenceClassification,
@@ -161,8 +161,8 @@ class BertClassifier(BaseTextClassifier):
                 save_strategy="epoch",
                 save_total_limit=1,
                 report_to="none",
+                metric_for_best_model="f1",
                 eval_strategy="epoch",
-                metric_for_best_model="accuracy",
                 load_best_model_at_end=True,    # Best score *per epoch*
             )
 
@@ -178,7 +178,7 @@ class BertClassifier(BaseTextClassifier):
             )
 
             trainer.train()
-            val_score = trainer.evaluate()["eval_accuracy"]
+            val_score = trainer.evaluate()["eval_f1"]
 
         return val_score
 
@@ -229,10 +229,13 @@ class BertClassifier(BaseTextClassifier):
         """Compute metrics for evaluation"""
         logits, labels = eval_pred
         preds = logits.argmax(axis=-1)
-
+        
         acc = accuracy_score(labels, preds)
         f1 = f1_score(labels, preds, average='weighted')
-        return {"accuracy": acc, "f1": f1}
+        precision = precision_score(labels, preds, average='weighted')
+        recall = recall_score(labels, preds, average='weighted')
+        
+        return {"accuracy": acc, "f1": f1, "precision": precision, "recall": recall}
 
     def predict(self, text, return_decoded=False, output=False):
         self.best_model.eval()  # evaluation mode