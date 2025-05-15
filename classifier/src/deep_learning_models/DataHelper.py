7f8e0eeff80898353e870500bebb8a7883078893
Tomer <tomer.barak2@gmail.com>
2025-05-15 21:25:28 +0300

    Deep Models run with new BaseTextClassifier, need to adjust with normalization and preprocessing

---
+++ classifier/src/deep_learning_models/DataHelper.py
@@ -0,0 +1,21 @@
+from classifier.src.classifiers.BaseTextClassifier import BaseTextClassifier
+
+class DataHelper(BaseTextClassifier):
+    def __init__(self):
+        super().__init__(labels=["antisemitic", "not_antisemitic"], seed=42)
+
+    def preprocess(self, datasets):
+        pass
+
+    def train(self, *args):
+        pass
+
+    def predict(self, text):
+        pass
+
+    def save_model(self):
+        pass
+
+    @staticmethod
+    def load_model(path):
+        pass
